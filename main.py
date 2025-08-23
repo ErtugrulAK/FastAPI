68c80f8fb091313b138a626737d6c532ed516885
Ertuğrul AK <ertugrul14ak@gmail.com>
2025-08-23 17:46:32 +0300

    Swagger , Redoc and Endpoint

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+import random
 
 app = FastAPI()
 
@@ -7,3 +8,24 @@ app = FastAPI()
 def read_root():
     return {"output":"Hello World"}
 
+@app.get("/{person}")
+def quote(person: str):
+    quotes={
+        "Edison": "Genius is one percent inspiration and ninety-nine percent perspiration.",
+        "Tesla": "If you want to find the secrets of the universe, think in terms of energy, frequency and vibration.",
+        "Einstein": "Imagination is more important than knowledge.",
+        "Newton": "If I have seen further it is by standing on the shoulders of Giants."
+    }
+    if person == "random":
+        temp = random.choice(list(quotes.values()))
+    else:
+        temp = quotes.get(person)
+
+
+
+    #return quotes.get(person)
+
+    return {"output": temp}
+
+
+    
